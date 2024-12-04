abadbaaa3a232a49209ac4ff90aed9b0a7641aed
invader237 <diego.trivino.fr@gmail.com>
2024-12-04 21:37:18 +0100

    :sparkles:(service): add method for WeatherDataDisplay conversion

--- src/main/java/com/example/project/service/WeatherService.java
+++ src/main/java/com/example/project/service/WeatherService.java
@@ -1,5 +1,7 @@
 package com.example.project.service;
 
+import com.example.project.model.WeatherResponse;
+import com.example.project.model.WeatherDataDisplay;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,20 +12,24 @@ public class WeatherService {
     @Value("${openweathermap.api.key}")
     private String apiKey;
 
-    public String getWeather(double lat, double lon) {
+    public WeatherResponse getWeather(double lat, double lon) {
         String url = String.format(
             "https://api.openweathermap.org/data/2.5/weather?lat=%f&lon=%f&units=metric&appid=%s",
             lat, lon, apiKey
         );
 
         RestTemplate restTemplate = new RestTemplate();
-        return restTemplate.getForObject(url, String.class);
+        return restTemplate.getForObject(url, WeatherResponse.class);  
     }
 
-    public String getWeatherDisplayData(double lat, double lon) {
-        // To do: Parse the JSON response and return only the data you want to display
-        return getWeather(lat,lon);
+    public WeatherDataDisplay getWeatherDisplayData(double lat, double lon) {
+        WeatherResponse weatherResponse = getWeather(lat, lon);
+
+        double temperature = weatherResponse.getMain().getTemp();
+        String description = weatherResponse.getWeather()[0].getDescription();
+        int humidity = weatherResponse.getMain().getHumidity();
+        double windSpeed = weatherResponse.getWind().getSpeed();
+
+        return new WeatherDataDisplay(temperature, description, humidity, windSpeed);
     }
-    
-    
 }
