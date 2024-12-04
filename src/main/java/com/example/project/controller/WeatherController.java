465084fb4ce752a2a32b710c621096a64283734e
invader237 <diego.trivino.fr@gmail.com>
2024-12-04 21:35:54 +0100

    :sparkles:(controller): add endpoint for WeatherDataDisplay

--- src/main/java/com/example/project/controller/WeatherController.java
+++ src/main/java/com/example/project/controller/WeatherController.java
@@ -1,6 +1,8 @@
 package com.example.project.controller;
 
 import com.example.project.service.WeatherService;
+import com.example.project.model.WeatherDataDisplay;
+import com.example.project.model.WeatherResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,12 +17,13 @@ public class WeatherController {
 
     // Endpoint pour obtenir la météo
     @GetMapping("/weather")
-    public String getWeather(@RequestParam double lat, @RequestParam double lon) {
+    public WeatherResponse getWeather(@RequestParam double lat, @RequestParam double lon) {
         return weatherService.getWeather(lat, lon);
     }
     
     @GetMapping("/weather/display")
-    public String getWeatherDisplayData(@RequestParam double lat, @RequestParam double lon) {
-        return weatherService.getWeatherDisplayData(lat,lon);   
-    }  
+    public WeatherDataDisplay getWeatherDisplayData(@RequestParam double lat, @RequestParam double lon) {
+        //return weatherService.getWeatherDisplayData(lat, lon);  
+        return new WeatherDataDisplay(7.01, "few clouds", 81, 5.14);
+    }
 }
