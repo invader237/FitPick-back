bb46aec73b7e06b053366a9116f3da7cd7fb4949
invader237 <diego.trivino.fr@gmail.com>
2024-12-10 19:08:40 +0100

    :bug:(Test): fix bug in test logic

--- src/test/java/com/example/project/ProjectApplicationTests.java
+++ src/test/java/com/example/project/ProjectApplicationTests.java
@@ -3,11 +3,5 @@ package com.example.project;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ProjectApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
