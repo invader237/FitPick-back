0bc51d99bbbf028604fcb5e15375c1e8702fd9ab
invader237 <diego.trivino.fr@gmail.com>
2024-12-15 01:21:14 +0100

    :goal_net:(Service): add exception handling for most services

--- src/main/java/com/example/project/service/clothingLib/ClothingService.java
+++ src/main/java/com/example/project/service/clothingLib/ClothingService.java
@@ -1,5 +1,7 @@
 package com.example.project.service.clothingLib;
 
+import com.example.project.dto.clothingLib.ClothingDTO;
+import com.example.project.dto.clothingLib.TagDTO;
 import com.example.project.modele.clothingLib.Clothing;
 import com.example.project.repository.clothingLib.ClothingRepository;
 import com.example.project.modele.clothingLib.Tag;
@@ -8,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 @Service
 public class ClothingService {
@@ -26,7 +28,11 @@ public class ClothingService {
      * @return A list of {@link Clothing} objects belonging to the user.
      */
     public List<Clothing> getClothingByUserId(Long userId) {
-        return clothingRepository.findByUserId(userId);
+        try {
+            return clothingRepository.findByUserId(userId);
+        } catch (Exception e) {
+            throw new RuntimeException("An error occurred while retrieving clothing items for user ID " + userId, e);
+        }
     }
 
     /**
@@ -46,26 +52,41 @@ public class ClothingService {
      * @param userId The ID of the user who owns the clothing item.
      * @param cloId  The ID of the clothing item whose tags are to be retrieved.
      * @return A list of {@link Tag} objects associated with the specified clothing item.
+     * @throws IllegalArgumentException if the clothing item does not exist.
      */
     public List<Tag> getTagsByClothingId(Long userId, Long cloId) {
-        return clothingRepository.findTagsByCloId(userId, cloId);
+        try {
+            Clothing clothing = getClothingById(userId, cloId);
+            return clothing.getTags();
+        } catch (Exception e) {
+            throw new RuntimeException("An error occurred while retrieving tags for clothing item with ID " + cloId + " and user ID " + userId, e);
+        }
     }
 
     /**
      * Add a new clothing item to the database.
      *
-     * @param clothing The {@link Clothing} object to add.
+     * @param clothingName The name of the clothing item.
+     * @param userId The ID of the user who owns the clothing.
+     * @param tagIds The IDs of the tags to associate with the clothing item.
      * @return The newly added {@link Clothing} object.
      */
     public Clothing addClothingWithTags(String clothingName, Long userId, List<Long> tagIds) {
-        List<Tag> tags = tagRepository.findAllById(tagIds);
+        try {
+            List<Tag> tags = tagRepository.findAllById(tagIds);
+            if (tags.size() != tagIds.size()) {
+                throw new IllegalArgumentException("Some tags could not be found with IDs: " + tagIds);
+            }
 
-        Clothing clothing = new Clothing();
-        clothing.setClo_lib(clothingName);
-        clothing.setUserId(userId);
-        clothing.setTags(tags);
+            Clothing clothing = new Clothing();
+            clothing.setClo_lib(clothingName);
+            clothing.setUserId(userId);
+            clothing.setTags(tags);
 
-        return clothingRepository.save(clothing);
+            return clothingRepository.save(clothing);
+        } catch (Exception e) {
+            throw new RuntimeException("An error occurred while adding the clothing with name " + clothingName + " for user ID " + userId, e);
+        }
     }
 
     /**
@@ -78,16 +99,23 @@ public class ClothingService {
      * @return The updated {@link Clothing} object.
      */
     public Clothing updateClothingWithTags(Long clothingId, String newName, Long userId, List<Long> tagIds) {
-        Clothing clothing = clothingRepository.findById(clothingId)
-            .orElseThrow(() -> new RuntimeException("Clothing not found with ID: " + clothingId));
+        try {
+            Clothing clothing = clothingRepository.findById(clothingId)
+                    .orElseThrow(() -> new IllegalArgumentException("Clothing not found with ID: " + clothingId));
 
-        clothing.setClo_lib(newName);
-        clothing.setUserId(userId);
+            clothing.setClo_lib(newName);
+            clothing.setUserId(userId);
 
-        List<Tag> tags = tagRepository.findAllById(tagIds);
-        clothing.setTags(tags);
+            List<Tag> tags = tagRepository.findAllById(tagIds);
+            if (tags.isEmpty() && !tagIds.isEmpty()) {
+                throw new IllegalArgumentException("Some tags could not be found with IDs: " + tagIds);
+            }
+            clothing.setTags(tags);
 
-        return clothingRepository.save(clothing);
+            return clothingRepository.save(clothing);
+        } catch (Exception e) {
+            throw new RuntimeException("An error occurred while updating the clothing with ID " + clothingId + " for user ID " + userId, e);
+        }
     }
 
     /**
@@ -95,13 +123,17 @@ public class ClothingService {
      *
      * @param userId The ID of the user who owns the clothing item.
      * @param cloId  The ID of the clothing item to delete.
+     * @return The {@link Clothing} object that was deleted, or throws an exception if not found.
+     * @throws IllegalArgumentException if the clothing item does not exist.
      */
     public Clothing deleteClothing(Long userId, Long cloId) {
-        Clothing clothing = clothingRepository.findByUserIdAndCloId(userId, cloId);
-        if (clothing != null) {
-            clothingRepository.delete(clothing); 
+        try {
+            Clothing clothing = getClothingById(userId, cloId);
+            clothingRepository.delete(clothing);
+            return clothing;
+        } catch (Exception e) {
+            throw new RuntimeException("An error occurred while deleting the clothing with ID " + cloId + " for user ID " + userId, e);
         }
-        return clothing; 
     }
 
 }
