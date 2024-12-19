e8a470b7fa734a2a32c59907604183a703deafa3
HyMathew <mathieu.drissi@outlook.fr>
2024-12-19 06:53:29 +0100

    ✨(repository): enhance Role and User repositories with Javadoc comments and additional query methods

--- src/main/java/com/example/project/repository/Authentification/RoleRepository.java
+++ src/main/java/com/example/project/repository/Authentification/RoleRepository.java
@@ -2,12 +2,21 @@ package com.example.project.repository.Authentification;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import com.example.project.modele.Authentification.Role;
+import com.example.project.model.Authentification.Role;
 
 import java.util.Optional;
 
+/**
+ * Repository pour gérer les opérations sur l'entité Role.
+ */
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    /**
+     * Recherche un rôle par son nom.
+     *
+     * @param name Nom du rôle.
+     * @return Un Optional contenant le rôle s'il existe.
+     */
     Optional<Role> findByName(String name);
 }
