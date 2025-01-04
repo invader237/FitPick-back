d97c3bb63a90ba46225430324e69a7d075cb19cb
e3282u <guigui97457@gmail.com>
2025-01-04 15:38:12 +0100

    :sparkles:(OutfitDTO.java): add validation to OutfitDTO

--- src/main/java/com/example/project/dto/outfitLib/OutfitDTO.java
+++ src/main/java/com/example/project/dto/outfitLib/OutfitDTO.java
@@ -1,22 +1,32 @@
-package com.example.project.dto.outfitLib; 
+package com.example.project.dto.outfitLib;
 
 import com.example.project.dto.clothingLib.ClothingDTO;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.util.List;
 
 public class OutfitDTO {
+
     private Long id;
+
+    @NotBlank(message = "Le nom de la tenue est obligatoire.")
     private String name;
+
+    @NotNull(message = "La liste des vêtements ne peut pas être nulle.")
+    @Size(min = 4, max = 4, message = "Une tenue doit contenir exactement 4 vêtements.")
     private List<ClothingDTO> clothingList;
 
-    // Constructor
+    public OutfitDTO() {
+    }
+
     public OutfitDTO(Long id, String name, List<ClothingDTO> clothingList) {
         this.id = id;
         this.name = name;
         this.clothingList = clothingList;
     }
 
-    // Getters and Setters
     public Long getId() {
         return id;
     }
@@ -40,6 +50,4 @@ public class OutfitDTO {
     public void setClothingList(List<ClothingDTO> clothingList) {
         this.clothingList = clothingList;
     }
-
 }
-
