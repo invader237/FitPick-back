03f2d50fa0bf5eeb62876d92eb4de924a0cec692
HyMathew <mathieu.drissi@outlook.fr>
2025-01-08 07:29:17 +0100

    :art:(WeatherController.java): update import statement for WeatherService to reflect new package structure

--- src/main/java/com/example/project/controller/WeatherController.java
+++ src/main/java/com/example/project/controller/WeatherController.java
@@ -1,13 +1,16 @@
 package com.example.project.controller;
 
-import com.example.project.service.WeatherService;
 import com.example.project.model.WeatherDataDisplay;
 import com.example.project.model.WeatherResponse;
+import com.example.project.service.Weather.WeatherService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.*;
+
+
 
 @RestController
 @RequestMapping("/api/weather")
