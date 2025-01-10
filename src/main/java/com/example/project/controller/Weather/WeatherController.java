660e90149af4ac7f6caa1535cacdb9f1462d493b
invader237 <diego.trivino.fr@gmail.com>
2025-01-10 16:30:01 +0100

    :art:(code): remove unnecessary line breaks

--- src/main/java/com/example/project/controller/Weather/WeatherController.java
+++ src/main/java/com/example/project/controller/Weather/WeatherController.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.*;
 
-
-
 @RestController
 @RequestMapping("/api/weather")
 @CrossOrigin(origins = "http://localhost:3000") 
